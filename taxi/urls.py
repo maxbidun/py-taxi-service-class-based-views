865dc4aea8cd4f4cd528af55489db71f29872399
Max <bidun.max@gmail.com>
2025-04-15 00:16:17 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from .views import index
+from .views import index, ManufacturerListView, CarListView, CarDetailView, DriverListView, DriverDetailView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("manufacturer/", ManufacturerListView.as_view(), name="manufacturer-list"),
+    path("car/", CarListView.as_view(), name="car-list"),
+    path("car/<int:pk>", CarDetailView.as_view(), name="car-detail"),
+    path("driver/", DriverListView.as_view(), name="driver-list"),
+    path("driver/<int:pk>", DriverDetailView.as_view(), name="driver-detail")
 ]
 
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from taxi.models import Driver, Car, Manufacturer
 
+from django.views import generic
+
 
 def index(request):
     """View function for the home page of the site."""
@@ -13,3 +15,33 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+
+
+class CarListView(generic.ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer")
+    paginate_by = 5
+    ordering = ["model"]
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    paginate_by = 5
+    ordering = ["first_name"]
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+
+    def get_queryset(self) -> Driver:
+        return Driver.objects.all().prefetch_related("cars")
